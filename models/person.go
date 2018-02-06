4f2e51c51e56550811d4633ce6ff707193b5f7a1
Thiago Marçal <thiagomarcal@dev01s-MacBook-Pro.local>
2018-02-06 15:27:57 -0200

    Initial

--- models/person.go
+++ models/person.go
@@ -1,11 +1,12 @@
 package models
 
+// Persons map teste
 var Persons = make(map[string]Person)
 
 func init() {
 	Persons["1"] = Person{ID: "1", Firstname: "Thiago", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}}
 	Persons["2"] = Person{ID: "2", Firstname: "Rodrigo", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}}
-	Persons["2"] = Person{ID: "3", Firstname: "Emad", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}}
+	Persons["3"] = Person{ID: "3", Firstname: "Emad", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}}
 	Persons["4"] = Person{ID: "4", Firstname: "Ubira", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}}
 	Persons["5"] = Person{ID: "5", Firstname: "Valeria", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}}
 }
